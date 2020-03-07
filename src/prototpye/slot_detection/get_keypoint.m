b54d14a90f2870a7b6c07321a1eb48e7a461d2df
Darui Zhang <neozdr@gmail.com>
2020-03-07 17:13:42 +0800

    test hough

--- src/prototpye/slot_detection/get_keypoint.m
+++ src/prototpye/slot_detection/get_keypoint.m
@@ -1,26 +1,49 @@
 % use tample get line 
-%A = imread('dst.bmp')
-A = imread('canny.bmp')
+%scr = imread('dst.bmp')
+src = imread('canny.bmp');
 
 %load('')
 % display image
-imshow(A)
-imshow(gabor)
+%imshow(src)
+%imshow(gabor)
+
+% input: marker point
+% output
+pt1 = [17, 10];
+pt2 = [187, 10];
+stride = 10;
+
+RGB = insertShape(src,'circle',[pt1(1) pt1(2) 2],'LineWidth',2); % center x,y, radius
+RGB = insertShape(RGB,'circle',[pt2(1) pt2(2) 2],'LineWidth',2); % center x,y, radius
+
+imshow(RGB)
+[len,width] = size(src)
 
 
-point_left = [17, 10];
-point_right = [187, 10];
+window_width
+offset = 5
 
-stride = 10
+win_l = pt1[1]-5:pt1[1]+5
+win_r = pt2[1]-5:pt2[1]+5
+n = 30
+scan = src(win_l,:)
 
-x = 10
-y = 10
-B = A(x:x+24,y:y+24)
-imshow(B)
-z = conv2(gabor,B)
-z = corr2(gabor,B)
+for x = 1:len/10
+    
+end
+
+src_tilt = imrotate(src,35,'bilinear');
+imshowpair(scr,src_tilt,'montage')
+
+
+% x = 10
+% y = 10
+% B = A(x:x+24,y:y+24)
+% imshow(B)
+% z = conv2(gabor,B)
+% z = corr2(gabor,B)
 % for x = 10:170
 %     z = gabor*B
 % end
-kernel = gabor
-blurredImage = conv2(A, kernel, 'same');
+% kernel = gabor
+% blurredImage = conv2(A, kernel, 'same');
---
+++ test_hough.m
@@ -0,0 +1,42 @@
+%mathworks.com/help/images/ref/houghlines.html
+% test hough peak
+I  = imread('circuit.tif');
+BW = edge(imrotate(I,50,'crop'),'canny');
+[H,T,R] = hough(BW);
+P  = houghpeaks(H,2);
+imshow(H,[],'XData',T,'YData',R,'InitialMagnification','fit');
+xlabel('\theta'), ylabel('\rho');
+axis on, axis normal, hold on;
+plot(T(P(:,2)),R(P(:,1)),'s','color','white');
+
+% test hough line
+close all
+rotI = imrotate(I,33,'crop');
+BW = edge(rotI,'canny');
+[H,T,R] = hough(BW);
+imshow(H,[],'XData',T,'YData',R,...
+            'InitialMagnification','fit');
+xlabel('\theta'), ylabel('\rho');
+axis on, axis normal, hold on;
+P  = houghpeaks(H,5,'threshold',ceil(0.3*max(H(:))));
+x = T(P(:,2)); y = R(P(:,1));
+plot(x,y,'s','color','white');
+
+lines = houghlines(BW,T,R,P,'FillGap',5,'MinLength',7);
+figure, imshow(rotI), hold on
+max_len = 0;
+for k = 1:length(lines)
+   xy = [lines(k).point1; lines(k).point2];
+   plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','green');
+
+   % Plot beginnings and ends of lines
+   plot(xy(1,1),xy(1,2),'x','LineWidth',2,'Color','yellow');
+   plot(xy(2,1),xy(2,2),'x','LineWidth',2,'Color','red');
+
+   % Determine the endpoints of the longest line segment
+   len = norm(lines(k).point1 - lines(k).point2);
+   if ( len > max_len)
+      max_len = len;
+      xy_long = xy;
+   end
+end
